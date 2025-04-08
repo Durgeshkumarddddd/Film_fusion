e77e1942ecfcd74bd6aaf366d53ce761ab961e02
Durgeshkumarddddd <durgeshkumarddddd123@gmail.com>
2025-04-09 00:01:06 +0530

    new

--- new_movies_hub/src/components/HorizontalCards.jsx
+++ new_movies_hub/src/components/HorizontalCards.jsx
@@ -2,6 +2,9 @@
 export default function HorizontalCards(){
     return (
         <div>
+            <div className="bg-red-400 w-[20%] h-[20%] ">
+
+            </div>
             
         </div>
     );
